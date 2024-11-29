7181894cac5317e51d244c6a40fa2259082ac9f2
Renato Veneroso <rveneroso@gmail.com>
2024-11-28 22:38:47 -0300

    Código da aula 135 - Buscando repositórios.

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import { React, useState, useCallback } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
+import api from '../../services/api';
+
 export default function Main() {
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    function handleInputChange(e) {
+        setNewRepo(e.target.value);
+    }
+
+    const handleSubmit = useCallback((e)=>{
+        e.preventDefault();
+
+        async function submit() {
+            const response = await api.get(`repos/${newRepo}`);
+    
+            const data = {
+                name: response.data.full_name,
+            }
+    
+            setRepositorios([...repositorios,data]);
+            setNewRepo('');
+        }
+
+        submit();
+
+    }, [newRepo, repositorios]);
+
     return(
         <Container>
             <h1>
@@ -11,8 +39,13 @@ export default function Main() {
                 Meus Repositórios
             </h1>
 
-            <Form>
-                <input type="text" placeholder="Adicionar Repositórios"/>
+            <Form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Adicionar Repositórios"
+                    value={newRepo}
+                    onChange={handleInputChange}
+                />
 
                 <SubmitButton>
                     <FaPlus color="#FFF" size={14}/>
